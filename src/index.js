0ca6953b7ecd4e715f15d377175e50610137aa7d
MichalRzeszot <134526382+MichalRzeszot@users.noreply.github.com>
2023-12-10 20:00:58 +0100

    feat: init commit

--- src/index.js
+++ src/index.js
@@ -3,21 +3,45 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 const breedSelector = document.querySelector('.breed-select');
 const catInfoContainer = document.querySelector('.cat-info');
 const loaderElement = document.querySelector('.loader');
-const errorSpanElement = document.querySelector('.error');
+const errorMessageElement = document.querySelector('.error');
+errorMessageElement.classList.add('error-message');
 
-errorSpanElement.textContent = '';
-loaderElement.textContent = '';
-loaderElement.style.display = 'none';
-loaderElement.style.scale = '1';
+let isFirstLoad = true;
+
+function setLoadingState(isBreedsLoading, isCatLoading) {
+  if (isBreedsLoading) {
+    if (!isFirstLoad) {
+      loaderElement.textContent = 'Loading breeds, please wait...';
+      loaderElement.style.display = 'block';
+      document.body.classList.add('breeds-loading');
+    } else {
+      loaderElement.textContent = '';
+      loaderElement.style.display = 'none';
+      isFirstLoad = false;
+    }
+  } else if (isCatLoading) {
+    loaderElement.textContent = 'Loading cat information, please wait...';
+    loaderElement.style.display = 'block';
+    document.body.classList.add('cat-loading');
+  } else {
+    loaderElement.textContent = '';
+    loaderElement.style.display = 'none';
+    document.body.classList.remove('breeds-loading', 'cat-loading');
+  }
+  errorMessageElement.style.display = 'none';
+}
 
 fetchBreeds()
   .then(breeds => {
+    setLoadingState(true, false);
     const breedOptionsMarkup = breeds.map(
       breed => `<option value="${breed.id}">${breed.name}</option>`
     );
     breedSelector.innerHTML = breedOptionsMarkup.join('');
   })
   .catch(error => {
+    setLoadingState(false, false);
+    errorMessageElement.style.display = 'block';
     Notiflix.Report.failure(
       'Error',
       'Oops! Something went wrong! Try reloading the page!'
@@ -27,7 +51,7 @@ fetchBreeds()
 breedSelector.addEventListener('change', event => {
   const selectedBreedId = event.target.value;
   catInfoContainer.innerHTML = '';
-  loaderElement.style.display = 'block';
+  setLoadingState(false, true);
 
   fetchCatByBreed(selectedBreedId)
     .then(catData => {
@@ -39,12 +63,14 @@ breedSelector.addEventListener('change', event => {
       catInfoContainer.innerHTML = catHtml.join('');
     })
     .catch(error => {
+      setLoadingState(false, false);
+      errorMessageElement.style.display = 'block';
       Notiflix.Report.failure(
         'Error',
         'Oops! Something went wrong! Try reloading the page!'
       );
     })
-    .finally(() => {
-      loaderElement.style.display = 'none';
+    .finally(function () {
+      setLoadingState(false, false);
     });
 });
